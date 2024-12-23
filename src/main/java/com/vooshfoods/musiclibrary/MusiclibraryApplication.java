5ade1380fb027f7e4accd11cf3187832327f84c9
vandanakaviti <kavitivandana@gmail.com>
2024-12-23 19:30:30 +0530

    First commit

---
+++ src/main/java/com/vooshfoods/musiclibrary/MusiclibraryApplication.java
@@ -0,0 +1,13 @@
+package com.vooshfoods.musiclibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiclibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiclibraryApplication.class, args);
+	}
+
+}
